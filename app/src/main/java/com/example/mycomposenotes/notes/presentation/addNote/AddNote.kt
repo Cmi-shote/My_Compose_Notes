09cf12183472fb28d458d75337b63de2ab696d1a
similoluwashote <simi.shote@babbangona.com>
2024-12-14 20:31:55 +0100

    #Added add notes screen

---
+++ app/src/main/java/com/example/mycomposenotes/notes/presentation/addNote/AddNote.kt
@@ -0,0 +1,26 @@
+package com.example.mycomposenotes.notes.presentation.addNote
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import com.example.mycomposenotes.notes.presentation.noteDetails.NoteContent
+
+@Composable
+fun AddNote(modifier: Modifier = Modifier) {
+    val statusBarPadding = WindowInsets.statusBars.asPaddingValues()
+    NoteContent(
+        modifier = modifier
+            .padding(
+                start = statusBarPadding.calculateLeftPadding(LayoutDirection.Ltr) + 16.dp,
+                top = statusBarPadding.calculateTopPadding(),
+                end = statusBarPadding.calculateRightPadding(LayoutDirection.Ltr) + 16.dp,
+                bottom = statusBarPadding.calculateBottomPadding()
+            ),
+        note = null
+    )
+}
