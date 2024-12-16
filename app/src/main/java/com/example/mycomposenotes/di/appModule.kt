1547e58653f81264b5a930af8daa622950a3a734
similoluwashote <simi.shote@babbangona.com>
2024-12-16 12:38:34 +0100

    #Added Koin di and room database

--- app/src/main/java/com/example/mycomposenotes/di/appModule.kt
+++ app/src/main/java/com/example/mycomposenotes/di/appModule.kt
@@ -2,9 +2,11 @@ package com.example.mycomposenotes.di
 
 import com.example.mycomposenotes.notes.data.NotesRepo
 import com.example.mycomposenotes.notes.data.NotesRepoImpl
+import com.example.mycomposenotes.notes.presentation.listNotes.ListNotesViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     single<NotesRepo> { NotesRepoImpl(get()) }
-    vi
+    viewModel { ListNotesViewModel(get()) }
 }
---
+++ app/src/main/java/com/example/mycomposenotes/notes/presentation/listNotes/ListNotesViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.mycomposenotes.notes.presentation.listNotes
+
+import androidx.lifecycle.ViewModel
+import com.example.mycomposenotes.notes.data.NotesRepo
+
+class ListNotesViewModel(private val repo: NotesRepo) : ViewModel() {
+
+}
